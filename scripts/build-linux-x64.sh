c0777e486b07122d4ac98feb9c6fcf2f00f3fa94
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-10-16 17:39:46 -0700

    Add linux-x64 artifacts and leptonica submodule

---
+++ scripts/build-linux-x64.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTS_DIR="$( dirname -- "$0"; )"
+BASE_DIR="$SCRIPTS_DIR/.."
+SOURCES_DIR="$BASE_DIR/sources"
+ARTIFACTS_DIR="$BASE_DIR/artifacts"
+
+TARGET_DIR="$ARTIFACTS_DIR/linux-x64"
+
+pushd "$SOURCES_DIR/leptonica"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -DBUILD_SHARED_LIBS=OFF
+cmake --build .
+sudo cmake --install .
+popd
+popd
+
+pushd "$SOURCES_DIR/tesseract"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DDISABLED_LEGACY_ENGINE=ON -DBUILD_TRAINING_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON
+cmake --build .
+popd
+popd
+
+cp "$SOURCES_DIR/tesseract/build/bin/tesseract" "$TARGET_DIR/tesseract"
+cp "$SOURCES_DIR/tesseract/AUTHORS" "$TARGET_DIR/../tesseract-authors.txt"
