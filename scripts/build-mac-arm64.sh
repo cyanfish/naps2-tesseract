10fcc2b1c54f969e8f5ff728fc95e43f03702518
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-10-21 21:03:40 -0700

    Full isolated build for Mac & fix OS target

--- scripts/build-mac-arm64.sh
+++ scripts/build-mac-arm64.sh
@@ -9,16 +9,43 @@ ARTIFACTS_DIR="$BASE_DIR/artifacts"
 
 TARGET_DIR="$ARTIFACTS_DIR/mac-arm64"
 
+pushd "$SOURCES_DIR/libjpeg-turbo"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DWITH_JPEG8=1
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/zlib"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/libpng"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DPNG_BUILD_ZLIB=ON -DZLIB_LIBRARIES="$( realpath "../../zlib/build/libz.a"; )"
+cmake --build .
+popd
+popd
+
 pushd "$SOURCES_DIR/leptonica"
 rm -rf build
 mkdir build
 pushd "build"
-cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
-    -DJPEG_LIBRARY="/opt/homebrew/lib/libjpeg.a" -DJPEG_INCLUDE_DIR="/opt/homebrew/include/" \
-    -DPNG_LIBRARY="/opt/homebrew/lib/libpng16.a" -DPNG_INCLUDE_DIR="/opt/homebrew/include/libpng16/" \
-    -DZLIB_LIBRARY="/opt/homebrew/opt/zlib/lib/libz.a" -DZLIB_INCLUDE_DIR="/opt/homebrew/opt/zlib/include/"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DJPEG_LIBRARY="$( realpath "../../libjpeg-turbo/build/libjpeg.a"; )" -DJPEG_INCLUDE_DIR="$( realpath "../../libjpeg-turbo/"; );$( realpath "../../libjpeg-turbo/build/"; )" \
+    -DPNG_LIBRARY="$( realpath "../../libpng/build/libpng16.a"; )" -DPNG_INCLUDE_DIR="$( realpath "../../libpng/build/"; )" \
+    -DZLIB_LIBRARY="$( realpath "../../zlib/build/libz.a"; )" -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/build/"; )"
 cmake --build .
-sudo cmake --install .
 popd
 popd
 
@@ -26,15 +53,16 @@ pushd "$SOURCES_DIR/tesseract"
 rm -rf build
 mkdir build
 pushd "build"
-cmake .. -G"Unix Makefiles" \
+cmake .. -G"Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
     -DDISABLED_LEGACY_ENGINE=ON -DBUILD_TRAINING_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON
+    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON \
+    -DLeptonica_DIR="$( realpath "../../leptonica/build/"; )"
 cmake --build .
 popd
 popd
 
 # Note: Had to manually remove gif, tiff, etc. (everything but jpeg/png/zlib) from leptonica CMakeLists
-# TODO: Use -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 for all builds
+# Brew dependencies: cmake coreutils
 
 cp "$SOURCES_DIR/tesseract/build/bin/tesseract" "$TARGET_DIR/tesseract"
 cp "$SOURCES_DIR/tesseract/AUTHORS" "$TARGET_DIR/../tesseract-authors.txt"
--- scripts/build-mac-x64.sh
+++ scripts/build-mac-x64.sh
@@ -9,17 +9,47 @@ ARTIFACTS_DIR="$BASE_DIR/artifacts"
 
 TARGET_DIR="$ARTIFACTS_DIR/mac-x64"
 
+pushd "$SOURCES_DIR/libjpeg-turbo"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DWITH_JPEG8=1 \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_C_FLAGS="-arch x86_64"
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/zlib"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_C_FLAGS="-arch x86_64"
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/libpng"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DPNG_BUILD_ZLIB=ON -DZLIB_LIBRARIES="$( realpath "../../zlib/build/libz.a"; )" \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_C_FLAGS="-arch x86_64"
+cmake --build .
+popd
+popd
+
 pushd "$SOURCES_DIR/leptonica"
 rm -rf build
 mkdir build
 pushd "build"
-cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
-    -DJPEG_LIBRARY="/usr/local/lib/libjpeg.a" -DJPEG_INCLUDE_DIR="/usr/local/include/" \
-    -DPNG_LIBRARY="/usr/local/lib/libpng16.a" -DPNG_INCLUDE_DIR="/usr/local/include/libpng16/" \
-    -DZLIB_LIBRARY="/usr/local/opt/zlib/lib/libz.a" -DZLIB_INCLUDE_DIR="/usr/local/opt/zlib/include/" \
-    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_CXX_FLAGS="-arch x86_64"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DJPEG_LIBRARY="$( realpath "../../libjpeg-turbo/build/libjpeg.a"; )" -DJPEG_INCLUDE_DIR="$( realpath "../../libjpeg-turbo/"; );$( realpath "../../libjpeg-turbo/build/"; )" \
+    -DPNG_LIBRARY="$( realpath "../../libpng/build/libpng16.a"; )" -DPNG_INCLUDE_DIR="$( realpath "../../libpng/build/"; )" \
+    -DZLIB_LIBRARY="$( realpath "../../zlib/build/libz.a"; )" -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/build/"; )" \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_C_FLAGS="-arch x86_64" -DCMAKE_CXX_FLAGS="-arch x86_64"
 cmake --build .
-sudo cmake --install .
 popd
 popd
 
@@ -27,20 +57,20 @@ pushd "$SOURCES_DIR/tesseract"
 rm -rf build
 mkdir build
 pushd "build"
-cmake .. -G"Unix Makefiles" \
+cmake .. -G"Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
     -DDISABLED_LEGACY_ENGINE=ON -DBUILD_TRAINING_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON  \
-    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_SYSTEM_PROCESSOR=AMD64 \
-    -DCMAKE_CXX_FLAGS="-arch x86_64"
+    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON \
+    -DLeptonica_DIR="$( realpath "../../leptonica/build/"; )" \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_CXX_FLAGS="-arch x86_64"
 cmake --build .
 popd
 popd
 
-# Note: Had to install Homebrew and libpng/jpeg-turbo/zlib x64
-# See https://gist.github.com/progrium/b286cd8c82ce0825b2eb3b0b3a0720a0#homebrew
-# I also couldn't figure out how to successfully set CMAKE_SYSTEM_PROCESSOR so I manually changed it in CMakeLists.txt
-# Also added "-arch x86_64" manually to CMAKE_CXX_FLAGS_RELEASE after -Wno-unused-command-line-argument
-# TODO: Use -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 for all builds
+# Note: Had to manually remove gif, tiff, etc. (everything but jpeg/png/zlib) from leptonica CMakeLists
+# Brew dependencies: cmake coreutils nasm
+#
+# I also couldn't figure out how to successfully set CMAKE_SYSTEM_PROCESSOR so I manually changed it in Tesseract's
+# CMakeLists.txt with tesseract-xcomplile.patch
 
 cp "$SOURCES_DIR/tesseract/build/bin/tesseract" "$TARGET_DIR/tesseract"
 cp "$SOURCES_DIR/tesseract/AUTHORS" "$TARGET_DIR/../tesseract-authors.txt"
