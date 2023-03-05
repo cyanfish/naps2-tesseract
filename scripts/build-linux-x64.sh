4d349ad8a92b5434c8a27bbcfb2f060cff0f4351
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-03-05 20:29:55 +0000

    Full static build for linux

--- scripts/build-linux-x64.sh
+++ scripts/build-linux-x64.sh
@@ -9,11 +9,42 @@ ARTIFACTS_DIR="$BASE_DIR/artifacts"
 
 TARGET_DIR="$ARTIFACTS_DIR/linux-x64"
 
+pushd "$SOURCES_DIR/libjpeg-turbo"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DWITH_JPEG8=1
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/zlib"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/libpng"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
+    -DPNG_BUILD_ZLIB=ON -DZLIB_LIBRARIES="$( realpath "../../zlib/build/libz.a"; )"
+cmake --build .
+popd
+popd
+
 pushd "$SOURCES_DIR/leptonica"
 rm -rf build
 mkdir build
 pushd "build"
-cmake .. -DBUILD_SHARED_LIBS=OFF
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
+        -DJPEG_LIBRARY="$( realpath "../../libjpeg-turbo/build/libjpeg.a"; )" -DJPEG_INCLUDE_DIR="$( realpath "../../libjpeg-turbo/"; );$( realpath "../../libjpeg-turbo/build/"; )" \
+        -DPNG_LIBRARY="$( realpath "../../libpng/build/libpng16.a"; )" -DPNG_INCLUDE_DIR="$( realpath "../../libpng/build/"; )" \
+        -DZLIB_LIBRARY="$( realpath "../../zlib/build/libz.a"; )" -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/build/"; )"
 cmake --build .
 sudo cmake --install .
 popd
