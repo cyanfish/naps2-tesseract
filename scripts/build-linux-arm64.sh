ce4cbd2c8be4517f3b9f41740967d6bece5e54e2
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-07-06 16:02:53 -0700

    Add linux-arm64 build script

---
+++ scripts/build-linux-arm64.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTS_DIR="$( dirname -- "$0"; )"
+BASE_DIR="$SCRIPTS_DIR/.."
+SOURCES_DIR="$BASE_DIR/sources"
+ARTIFACTS_DIR="$BASE_DIR/artifacts"
+
+TARGET_DIR="$ARTIFACTS_DIR/linux-arm64"
+
+pushd "$SOURCES_DIR/libjpeg-turbo"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DWITH_JPEG8=1 \
+    -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/zlib"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/libpng"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
+    -DPNG_BUILD_ZLIB=ON -DZLIB_LIBRARIES="$( realpath "../../zlib/build/libz.a"; )" \
+    -DZLIB_INCLUDE_DIRS="$( realpath "../../zlib/"; );$( realpath "../../zlib/build/"; )" \
+    -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/"; );$( realpath "../../zlib/build/"; )" \
+    -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/leptonica"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
+        -DJPEG_LIBRARY="$( realpath "../../libjpeg-turbo/build/libjpeg.a"; )" -DJPEG_INCLUDE_DIR="$( realpath "../../libjpeg-turbo/"; );$( realpath "../../libjpeg-turbo/build/"; )" \
+        -DPNG_LIBRARY="$( realpath "../../libpng/build/libpng16.a"; )" -DPNG_PNG_INCLUDE_DIR="$( realpath "../../libpng/"; );$( realpath "../../libpng/build/"; )" \
+        -DZLIB_LIBRARY="$( realpath "../../zlib/build/libz.a"; )" -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/"; );$( realpath "../../zlib/build/;" )" \
+        -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++
+cmake --build .
+popd
+popd
+
+pushd "$SOURCES_DIR/tesseract"
+rm -rf build
+mkdir build
+pushd "build"
+cmake .. -G"Unix Makefiles" \
+    -DDISABLED_LEGACY_ENGINE=ON -DBUILD_TRAINING_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF \
+    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON \
+    -DLeptonica_DIR="$( realpath "../../leptonica/build/"; )" \
+    -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++
+cmake --build .
+popd
+popd
+
+cp "$SOURCES_DIR/tesseract/build/bin/tesseract" "$TARGET_DIR/tesseract"
+cp "$SOURCES_DIR/tesseract/AUTHORS" "$TARGET_DIR/../tesseract-authors.txt"
