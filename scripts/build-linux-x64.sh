a2156d61cc28e682af4eef54d4c93194c79eae91
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-07-06 16:02:34 -0700

    Fix linux build script

--- scripts/build-linux-x64.sh
+++ scripts/build-linux-x64.sh
@@ -46,9 +46,8 @@ pushd "build"
 cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF \
         -DJPEG_LIBRARY="$( realpath "../../libjpeg-turbo/build/libjpeg.a"; )" -DJPEG_INCLUDE_DIR="$( realpath "../../libjpeg-turbo/"; );$( realpath "../../libjpeg-turbo/build/"; )" \
         -DPNG_LIBRARY="$( realpath "../../libpng/build/libpng16.a"; )" -DPNG_PNG_INCLUDE_DIR="$( realpath "../../libpng/"; );$( realpath "../../libpng/build/"; )" \
-        -DZLIB_LIBRARY="$( realpath "../../zlib/build/libz.a"; )" -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/"; );$( realpath "../../zlib/build/; )"
+        -DZLIB_LIBRARY="$( realpath "../../zlib/build/libz.a"; )" -DZLIB_INCLUDE_DIR="$( realpath "../../zlib/"; );$( realpath "../../zlib/build/;" )"
 cmake --build .
-sudo cmake --install .
 popd
 popd
 
@@ -58,7 +57,8 @@ mkdir build
 pushd "build"
 cmake .. -G"Unix Makefiles" \
     -DDISABLED_LEGACY_ENGINE=ON -DBUILD_TRAINING_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON
+    -DDISABLE_CURL=ON -DDISABLE_ARCHIVE=ON -DGRAPHICS_DISABLED=ON -DENABLE_LTO=ON \
+    -DLeptonica_DIR="$( realpath "../../leptonica/build/"; )"
 cmake --build .
 popd
 popd
